4d7e97985e01968136bff10335ca6bc28bb8d000
nag-murali <93373775+nag-murali@users.noreply.github.com>
2022-01-19 13:22:54 +0530

    trying

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const ejs = require("ejs")
 const connect = () => {
     return mongoose.connect("mongodb+srv://murali:murali@web-test.6swdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 }
+
+app.set("viewengine", "ejs");
+app.get("/", (req, res) => {
+
+    res.render("home_page/home.ejs");
+})
+
+app.use(express.static("public"));
 
 app.listen(5354, async() => {
     try{
